dfca5a38764250fb70175807287ea424244780b7
Mykursins <edgechang@foxmail.com>
2017-09-01 16:55:35 +0800

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,9 +1,9 @@
 // Assembly Console, Version 2.0.1.401
 
-[assembly: System.Reflection.AssemblyProduct("Model UN Conference Management")]
+[assembly: System.Reflection.AssemblyProduct("Broddsúlan Conference Management")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 Broddsulan 2017\0\0\0\0\0\0")]
 [assembly: System.Reflection.AssemblyTrademark("")]
-[assembly: System.Reflection.AssemblyDescription("界碑模联会议控制中心")]
+[assembly: System.Reflection.AssemblyDescription("界碑会议控制中心")]
 [assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.DisableOptimizations | System.Diagnostics.DebuggableAttribute.DebuggingModes.EnableEditAndContinue | System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | System.Diagnostics.DebuggableAttribute.DebuggingModes.Default)]
 [assembly: System.Runtime.InteropServices.ComVisible(true)]
 [assembly: System.Reflection.AssemblyCompany("Bro\0\0\0\0模联项目部")]
